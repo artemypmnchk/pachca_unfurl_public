dd43651cffd0a88bce0fa3968ee16c6d7555b8f3
Artemy <artemypominchuk@MacBook-Air-Artemy.local>
2025-05-29 19:36:54 +0500

    Обновление файлов .replit и replit.nix для поддержки UTF-8 и правильной настройки сервера

--- replit.nix
+++ replit.nix
@@ -3,5 +3,13 @@
     pkgs.ruby_3_2
     pkgs.rubyPackages_3_2.puma
     pkgs.rubyPackages_3_2.sinatra
+    pkgs.rubyPackages_3_2.bundler
+    pkgs.glibc
+    pkgs.glibc.locales
   ];
+  env = {
+    LANG = "en_US.UTF-8";
+    LC_ALL = "en_US.UTF-8";
+    RUBYOPT = "-E UTF-8:UTF-8";
+  };
 }
